010ed08c0466ec128d00851e8b97e8725fb07c2c
mikksillaste <mikk.sillaste@gmail.com>
2017-09-12 23:14:55 +0300

    Initial commit

---
+++ ht1.py
@@ -0,0 +1,41 @@
+import agents
+
+clean_room_seen = False
+
+
+def MyVacuumAgent():
+    def program(percept):
+        global clean_room_seen
+        print(percept)
+        location, status = percept
+        # model[location] = status  # Update the model here
+        # if model[loc_A] == model[loc_B] == 'Clean':
+        #    return 'NoOp'
+        if status == 'Dirty':
+            print('Suck')
+            clean_room_seen = True
+            return 'Suck'
+        elif clean_room_seen:
+            return "NoOp"
+        elif location == agents.loc_A:
+            print('Right')
+            clean_room_seen = True
+            return 'Right'
+        elif location == agents.loc_B:
+            print('Left')
+            clean_room_seen = True
+            return 'Left'
+
+    return agents.Agent(program)
+
+e = agents.TrivialVacuumEnvironment()
+# e.add_thing(agents.ModelBasedVacuumAgent())
+agent1 = MyVacuumAgent()
+agent1 = agents.ModelBasedVacuumAgent()
+e.add_thing(agent1)
+
+e.status = {agents.loc_A: "Dirty", agents.loc_B: "Dirty"}
+print(e.status)
+e.run(10)
+print(e.status)
+print(agent1.performance)
