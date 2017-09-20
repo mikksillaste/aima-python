ef45167d803c4a61f4fa544e136572e8e428ad75
mikksillaste <mikk.sillaste@gmail.com>
2017-09-20 13:48:34 +0300

    Harjutus tund 3

--- ht3.py
+++ ht3.py
@@ -3,6 +3,10 @@ import search
 
 class MiniSudoku(search.Problem):
     def actions(self, state):
+        space = state.index(0)
+        row = space // 4
+        col = space % 4
+
         actions = []
 
         return actions
@@ -11,6 +15,14 @@ class MiniSudoku(search.Problem):
         newstate = list(state)
 
         return tuple(newstate)
+
+    def goal_test(self, state):
+        # blah asemel on lõppseisu tingimus
+        if blah:
+            return True
+
+        return False
+
 
 inistate = (2, 1, 0, 0,
             0, 3, 2, 0,
