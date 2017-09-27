68e8be75ca0602b959ea1fe1abc8af7b3668ed64
mikksillaste <mikk.sillaste@gmail.com>
2017-09-27 13:20:14 +0300

    Harjutus tund 4

---
+++ ht4.py
@@ -0,0 +1,71 @@
+import search
+
+class VacWorld(search.Problem):
+    def actions(self, state):
+        actions = []
+
+        if state["loc"] == "A":
+            if state["A"] == "Dirty":
+                actions.append("Suck")
+                actions.append("Right")
+            elif state["A"] == "Clean":
+                actions.append("NoOp")
+                actions.append("Right")
+        elif state["loc"] == "B":
+            if state["B"] == "Dirty":
+                actions.append("Suck")
+                actions.append("Right")
+                actions.append("Left")
+            elif state["B"] == "Clean":
+                actions.append("NoOp")
+                actions.append("Right")
+                actions.append("Left")
+        elif state["loc"] == "C":
+            if state["C"] == "Dirty":
+                actions.append("Suck")
+                actions.append("Right")
+                actions.append("Left")
+            elif state["C"] == "Clean":
+                actions.append("Right")
+                actions.append("Left")
+                actions.append("NoOp")
+        elif state["loc"] == "D":
+            if state["D"] == "Dirty":
+                actions.append("Suck")
+                actions.append("Left")
+            elif state["D"] == "Clean":
+                actions.append("NoOp")
+                actions.append("Left")
+
+        return actions
+
+    def result(self, state, action):
+        newstate = state.copy()
+        # action on yks nendest: "Left", "Right", "Suck", "NoOp"
+
+        return newstate
+
+    def goal_test(self, state):
+    # kui on kõik puhas suus return true
+        if state["A"] == "Clean" and state["B"] == "Clean" and state["C"] == "Clean" and state["D"] == "Clean":
+            return True
+    # muidu false
+        return False
+
+#algoleku kirjeldus 1
+algolek = {
+    "A" : "Dirty",
+    "B" : "Clean",
+    "C" : "Clean",
+    "D" : "Dirty",
+    "loc" : "C"
+}
+
+#algoleku kirjeldus 2
+#algolek = (1,0,0,1,2)
+
+p=VacWorld(algolek)
+# print(p.actions(olek))
+# asi = search.breadth_first_tree_search(p)
+# asi = search.iterative_deepening_search(p)
+# print(asi.solution())
