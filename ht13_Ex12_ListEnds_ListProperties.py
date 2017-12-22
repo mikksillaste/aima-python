9a39ab3067b33fd79a9ea10740a32a82340c079b
mikksillaste <mikk.sillaste@gmail.com>
2017-12-22 18:54:34 +0200

    eksamiks 4

--- ht13_Ex12_ListEnds_ListProperties.py
+++ ht13_Ex12_ListEnds_ListProperties.py
@@ -1,4 +1,12 @@
 a = [5, 10, 15, 20, 25]
+b = [11, 15, 176, 555, 43]
+
 
 # funktsioon mis teeb uue listi ainult esimesest ja viimasest arvust antud listis
 def newList(list):
+    newlist = [list[0], list[-1]]
+    return newlist
+
+
+print(newList(a))
+print(newList(b))
